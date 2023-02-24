e4b6a4dc3b6d4710d3a693c2fef38c8d5b17ce7f
Jayson Chin <jayson.chin@eversana.com>
2023-02-24 17:16:51 -0500

    first commit

---
+++ linode-terraform-web.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.27.1"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.jayson_terraform_token
+}
+resource "linode_instance" "terraform-dash" {
+        image = "linode/ubuntu22.04"
+        label = "dash-us-east"
+        group = "HNS-frontend"
+        region = "us-east"
+        type = "g6-nanode-1"
+        #tags = "frontend"
+        authorized_keys = [ var.ssh_keys_jayson ]
+        root_pass = var.root_pass
+}
+
+resource "linode_instance" "terraform-ns1" {
+        image = "linode/ubuntu22.04"
+        label = "ns1-us-east"
+        group = "HNS-backend"
+        region = "us-east"
+        type = "g6-nanode-1"
+        #tags = "frontend"
+        authorized_keys = [ var.ssh_keys_jayson ]
+        root_pass = var.root_pass
+}
+
+resource "linode_instance" "terraform-ns2" {
+        image = "linode/ubuntu22.04"
+        label = "ns2-us-central"
+        group = "HNS-backend"
+        region = "us-central"
+        type = "g6-nanode-1"
+        #tags = "frontend"
+        authorized_keys = [ var.ssh_keys_jayson ]
+        root_pass = var.root_pass
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "root_pass" {
+    type = string
+}
+variable "jayson_terraform_token" {
+    type = string
+}
+variable "ssh_keys_jayson" {
+    type = string
+}
